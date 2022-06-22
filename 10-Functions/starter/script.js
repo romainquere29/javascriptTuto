e7c7c9aa0733d85878969f6df2fe68dd82bd778a
Romain QUERE <romain.quere29@gmail.com>
2022-06-22 21:50:59 +0200

    finalize functions with closure

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -132,9 +132,192 @@ const airfrance = {
     //book = function() {}
 }
 
-book.call(airfrance, 943, 'Romain QUERE'); 
-book.apply(airfrance, [944, 'Romain QUERE']); //apply requires the arguments in an array
+// book.call(airfrance, 943, 'Romain QUERE');
+// book.apply(airfrance, [944, 'Romain QUERE']); //apply requires the arguments in an array
 
 
-console.log(lufthansa,eurowings,airfrance);
+// const bookEW = book.bind(eurowings);
+// bookEW(222, 'Romain De Quere');
 
+// const bookEW23 = book.bind(eurowings, 23);
+// bookEW23('Romain De Quere2');
+
+// const bookEW24 = book.bind(lufthansa,24);
+// bookEW24('Romain De Quere2');
+
+// console.log(lufthansa,eurowings,airfrance);
+
+// lufthansa.planes = 300;
+// lufthansa.buyPlane = function(){
+//     console.log(this);
+//     this.planes++;
+//     console.log(this.planes);
+// }
+
+// document.querySelector('.buy').addEventListener('click',lufthansa.buyPlane.bind(lufthansa));
+
+// const addTax = (rate,value) => value + value * rate;
+// console.log(addTax(0.1, 200));
+
+// const addVAT = addTax.bind(null, 0.196);
+// console.log(addVAT(200));
+
+
+// const customAddTax = function(rate) {
+//     return function(value) {
+//         return value + value * rate;
+//     } 
+// }
+
+// const addVAT2 = customAddTax(0.196);
+// console.log(addVAT2(200));
+
+///////////////////////////////////////
+// Coding Challenge #1
+
+/* 
+Let's build a simple poll app!
+
+A poll has a question, an array of options from which people can choose, and an array with the number of replies for each option. This data is stored in the starter object below.
+
+Here are your tasks:
+
+1. Create a method called 'registerNewAnswer' on the 'poll' object. The method does 2 things:
+  1.1. Display a prompt window for the user to input the number of the selected option. The prompt should look like this:
+        What is your favourite programming language?
+        0: JavaScript
+        1: Python
+        2: Rust
+        3: C++
+        (Write option number)
+  
+  1.2. Based on the input number, update the answers array. For example, if the option is 3, increase the value AT POSITION 3 of the array by 1. Make sure to check if the input is a number and if the number makes sense (e.g answer 52 wouldn't make sense, right?)
+2. Call this method whenever the user clicks the "Answer poll" button.
+3. Create a method 'displayResults' which displays the poll results. The method takes a string as an input (called 'type'), which can be either 'string' or 'array'. If type is 'array', simply display the results array as it is, using console.log(). This should be the default option. If type is 'string', display a string like "Poll results are 13, 2, 4, 1". 
+4. Run the 'displayResults' method at the end of each 'registerNewAnswer' method call.
+
+HINT: Use many of the tools you learned about in this and the last section 😉
+
+BONUS: Use the 'displayResults' method to display the 2 arrays in the test data. Use both the 'array' and the 'string' option. Do NOT put the arrays in the poll object! So what shoud the this keyword look like in this situation?
+
+BONUS TEST DATA 1: [5, 2, 3]
+BONUS TEST DATA 2: [1, 5, 3, 9, 6, 1]
+
+GOOD LUCK 😀
+*/
+const poll = {
+    question: 'What is your favourite programming language?',
+    options: ['0: JavaScript', '1: Python', '2: Rust', '3: C++'],
+    // This generates [0, 0, 0, 0]. More in the next section 😃
+    answers: new Array(4).fill(0),
+    registerNewAnswer : function () {
+        let answersStr = this.question + '\n';
+        for (const el of this.options) {
+            answersStr += el + '\n';
+        }
+        const UserAnswer=parseInt(prompt(answersStr));
+        // console.log(typeof UserAnswer === 'number');
+        // console.log(UserAnswer < this.answers.length);
+        // console.log(this.answers[UserAnswer]++);
+        typeof UserAnswer === 'number' && UserAnswer < this.answers.length && this.answers[UserAnswer]++;
+        this.displayResults('string');
+    },
+    displayResults : function(type='array') {
+        if (type === 'array') {
+            console.log(this.answers);
+        }
+        else if (type === 'string') {
+            console.log('Poll results are ' + this.answers.join(', '));
+        }
+    }
+};
+
+document.querySelector('.poll').addEventListener('click',poll.registerNewAnswer.bind(poll));
+
+// poll.displayResults.call({answers : [5, 2, 3]},'string');
+// poll.displayResults.call({answers : [1, 5, 3, 9, 6, 1]},'string');
+
+// const runOnce = function() {
+//     console.log('This will never run again');
+// };
+
+// // runOnce();
+
+// (function() {
+//     console.log('This will never run again 2');
+// })();
+
+// {
+//     const isPrivate = 23;
+//     var notPrivate = 46;
+// }
+
+// // console.log(isPrivate);
+// console.log(notPrivate);
+
+// const secureBooking = function() {
+//     let passengerCount = 0;
+//     return function() {
+//         passengerCount++;
+//         console.log(passengerCount);
+//     }
+// };
+
+// const booker = secureBooking();
+
+// booker();
+// A function has access to the var environment of the context on 
+// which they were created
+
+// booker();
+// booker();
+
+// let f;
+
+// const g = function() {
+//     const a = 23;
+//     f = function() {
+//         console.log(a*2);
+//     }
+// }
+
+// const h = function() {
+//     const b = 777;
+//     f = function() {
+//         console.log(b*2);
+//     }
+// }
+
+// g();
+// f();
+// console.dir(f);
+
+// //Reassign f()
+// h();
+// f();
+
+// console.dir(f);
+
+//Example 2
+// const boardPassengers = function(n,wait) {
+//     const perGroup = n/3;
+//     setTimeout(function(){
+//         console.log(`We are now baording all ${n} passengers`);
+//         console.log(`There are 3 groups, each with ${perGroup} passenger`);
+//     }, wait * 1000);
+//     console.log(`Will start boarding in ${wait} seconds`);
+// }
+
+// const perGroup = 6;
+// boardPassengers(180,5);
+
+( function () {
+    const header = document.querySelector('h1');
+    header.style.color='red';
+
+    document.querySelector('body').addEventListener('click', function() {
+            header.style.color='blue';
+        });
+})();
+
+// console.dir(window);

20940888918b0fca42fc823cd5cd9d5c5110e0da
Romain QUERE <rquere@tribvn-hc.com>
2022-06-29 18:01:47 +0200

    start arrays module : map + filter + reduce

--- 11-Arrays-Bankist/starter/script2.js
+++ 11-Arrays-Bankist/starter/script2.js
@@ -61,6 +61,70 @@ const inputLoanAmount = document.querySelector('.form__input--loan-amount');
 const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
+{/* <div class="movements__row">
+  <div class="movements__type movements__type--deposit">2 deposit</div>
+  <div class="movements__date">3 days ago</div>
+  <div class="movements__value">4 000€</div>
+</div> */}
+
+const displayMovements = function(movements) {
+
+  // console.log(containerMovements.innerHTML);
+  // console.log('-----------');
+  // console.log(containerMovements.textContent);
+  containerMovements.innerHTML = '';
+
+  movements.forEach(function(mov,i) {
+    const type = mov > 0 ? 'deposit' : 'withdrawal';
+
+    const html = `
+    <div class="movements__row">
+      <div class="movements__type movements__type--${type}">${i + 1} ${type}</div>
+      <div class="movements__value">${mov}</div>
+    </div> ` ;
+
+    containerMovements.insertAdjacentHTML('afterbegin', html);
+
+  })
+
+}
+
+displayMovements(account1.movements);
+
+// const user = 'Steven Thomas Williams';
+
+// let username = '';
+// user.toLowerCase().split(' ').forEach(function(word) {
+//   username+=word.slice(0,1);
+//   // username+=word[0];
+// });
+
+// const username2 = user.toLowerCase().split(' ').map(name => name[0]).join('');
+
+
+// // const username = user.slice(0,1);
+// console.log(username);
+// console.log(username2);
+
+const CreateUsernames = function (accts) {
+  accts.forEach(function(acc) {
+    acc.username = acc.owner.toLowerCase().split(' ').map(name => name[0]).join('');
+  })
+}
+
+console.log(CreateUsernames(accounts));
+// console.log(accounts);
+
+const calcPrintBalance = function(movements) {
+  const balance = movements.reduce((previous,current)=> previous + current, 0);
+  labelBalance.textContent = `${balance}€`;
+  console.log(labelBalance);
+}
+
+calcPrintBalance(account1.movements);
+
+
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
@@ -74,3 +138,151 @@ const currencies = new Map([
 const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
 
 /////////////////////////////////////////////////
+
+// for (const [i, movement] of movements.entries()) {
+//   if (movement > 0) {
+//     console.log(`Movement ${i + 1} : You deposited ${movement}`);
+//   } else {
+//     console.log(`Movement ${i + 1} : You withdrew ${movement * -1}`);
+//   }
+// }
+
+// movements.forEach(function(movement, index, array) {
+//   if (movement > 0) {
+//     console.log(`Movement ${index + 1} : You deposited ${movement}`);
+//   } else {
+//     console.log(`Movement ${index + 1} : You withdrew ${movement * -1}`);
+//   }
+// })
+
+// const currencies = new Map([
+//   ['USD', 'United States dollar'],
+//   ['EUR', 'Euro'],
+//   ['GBP', 'Pound sterling'],
+// ]);
+
+
+// currencies.forEach(function(value,key,map) {
+//   console.log(key + ' : ' +value);
+// });
+
+// const currenciesUnique = new Set(['USD', 'GBP', 'EUR', 'EUR', 'USD']);
+// currenciesUnique.forEach(function(value,key,map) {
+//   console.log(key + ' : ' +value);
+// });
+
+// Demo on bankist.netlify.app
+
+///////////////////////////////////////
+// Coding Challenge #1
+
+/* 
+Julia and Kate are doing a study on dogs. So each of them asked 5 dog owners about their dog's age, and stored the data into an array (one array for each). For now, they are just interested in knowing whether a dog is an adult or a puppy. A dog is an adult if it is at least 3 years old, and it's a puppy if it's less than 3 years old.
+
+Create a function 'checkDogs', which accepts 2 arrays of dog's ages ('dogsJulia' and 'dogsKate'), and does the following things:
+
+1. Julia found out that the owners of the FIRST and the LAST TWO dogs actually have cats, not dogs! So create a shallow copy of Julia's array, and remove the cat ages from that copied array (because it's a bad practice to mutate function parameters)
+2. Create an array with both Julia's (corrected) and Kate's data
+3. For each remaining dog, log to the console whether it's an adult ("Dog number 1 is an adult, and is 5 years old") or a puppy ("Dog number 2 is still a puppy 🐶")
+4. Run the function for both test datasets
+
+HINT: Use tools from all lectures in this section so far 😉
+
+TEST DATA 1: Julia's data [3, 5, 2, 12, 7], Kate's data [4, 1, 15, 8, 3]
+TEST DATA 2: Julia's data [9, 16, 6, 8, 3], Kate's data [10, 5, 6, 1, 4]
+
+GOOD LUCK 😀
+*/
+
+
+// const julia1 = [3, 5, 2, 12, 7];
+// const kate1 = [4, 1, 15, 8, 3];
+
+// const checkDogs = function(dogsJulia,dogsKate) {
+//   const dogsJuliaCorrected = dogsJulia.slice(1,-2);
+//   // console.log(dogsJulia);
+//   // console.log(dogsJuliaCorrected);
+//   const dogsMerged = dogsJuliaCorrected.concat(dogsKate);
+//   // console.log(dogsMerged);
+//   dogsMerged.forEach(function(age,index){
+//     const isAdutlt = age >= 3 ? true : false;
+//     if (isAdutlt) {
+//       console.log(`Dog number ${index + 1 } is an adult, and is ${age} years old `);
+//     }
+//     else {
+//       console.log(`Dog number ${index + 1} is still a puppy 🐶`);
+//     }
+//   })
+// };
+
+// checkDogs(julia1,kate1);
+// console.log(`-----------------------`);
+// checkDogs([9, 16, 6, 8, 3],[10, 5, 6, 1, 4]);
+
+
+// const movements = [200, 450, -400, 3000, -650, -130, 70, 1300];
+// const eurToUsd = 1.1;
+
+// const movementsUsd = movements.map(function(mov) {
+//   return mov * eurToUsd;
+// })
+// const movementsUsd = movements.map(mov => mov * eurToUsd);
+// console.log(movements);
+// console.log(movementsUsd);
+
+// const movementsUsd2 = [];
+// for (const element of movements) {
+//   movementsUsd2.push(element * eurToUsd);
+// }
+
+// console.log(movementsUsd2);
+
+// movements.forEach(function(movement, index, array) {
+//   if (movement > 0) {
+//     console.log(`Movement ${index + 1} : You deposited ${movement}`);
+//   } else {
+//     console.log(`Movement ${index + 1} : You withdrew ${movement * -1}`);
+//   }
+// })
+
+// const movementsDescription = movements.map((movement,index) => {
+//   if (movement > 0) {
+//       return `Movement ${index + 1} : You deposited ${movement}`;
+//     } else {
+//       return `Movement ${index + 1} : You withdrew ${movement * -1}`;
+//     }
+//   });
+
+// console.log(movementsDescription);
+
+// const deposit = movements.filter(function(mov) {
+//   return mov > 0;
+// });
+// const withdrawal = movements.filter(mov => mov < 0);
+// console.log(movements);
+// console.log(deposit);
+// console.log(withdrawal);
+
+// const deposit2 = [];
+// for (const mov of movements) {
+//   if (mov > 0) {
+//     deposit2.push(mov);
+//   }
+// }
+// console.log(deposit2);
+
+console.log(movements);
+
+const balance = movements.reduce(function(acc,cur,index,arr) {
+  console.log(`Iteration ${index} : ${acc}`)
+  return acc+cur;
+  // return acc.toString()+cur.toString();
+}, 0);
+console.log(balance);
+
+
+const balance2 = movements.reduce((acc,cur) => acc+cur, 0);
+console.log(balance2);
+
+const maxValue = movements.reduce((acc,cur) => acc > cur ? acc : cur, 0);
+console.log(`Max Value is ${maxValue}`);

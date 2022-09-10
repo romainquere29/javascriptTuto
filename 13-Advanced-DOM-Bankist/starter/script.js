bb41a25780397a0b4e87d118f978b4d0e58ad0da
Romain QUERE <rquere@tribvn-hc.com>
2022-09-10 13:06:58 +0200

    event propagation and event delegation in Market website

--- 13-Advanced-DOM-Bankist/starter/script.js
+++ 13-Advanced-DOM-Bankist/starter/script.js
@@ -29,3 +29,55 @@ document.addEventListener('keydown', function (e) {
     closeModal();
   }
 });
+
+////////////////////////////////////////////////
+//////// Page.navigation
+// document.querySelectorAll('.nav__link').forEach(function(el) {
+//   el.addEventListener('click', function(e) {
+//     e.preventDefault();
+//     const id = this.getAttribute('href');
+//     console.log(id);
+//     document.querySelector(id).scrollIntoView({behavior: 'smooth' });
+//   })
+// })
+
+document.querySelector('.nav__links').addEventListener('click', function(e) {
+  // console.log(e.target);
+  // Matching strategy
+  if(e.target.classList.contains('nav__link')) {
+    e.preventDefault();
+    const id = e.target.getAttribute('href');
+    // console.log(id);
+    document.querySelector(id).scrollIntoView({behavior: 'smooth' });
+  }
+
+})
+
+//191.Event propagation in Practise
+// rgb(255,255,255)
+const randomInt = (min,max) => Math.floor(Math.random() * (max - min + 1) + min);
+const randomColor = () => `rgb(${randomInt(0,255)},${randomInt(0,255)},${randomInt(0,255)})`;
+
+
+
+// document.querySelector('.nav__link').addEventListener('click', function(e) {
+//   // console.log('LINK');
+//   this.style.backgroundColor = randomColor();
+//   console.log('LINK', e.target, e.currentTarget);
+//   // Stop prpoagation
+//   // e.stopPropagation();
+// })
+
+
+
+// document.querySelector('.nav__links').addEventListener('click', function(e) {
+//   this.style.backgroundColor = randomColor();
+//   console.log('CONTAINER', e.target,  e.currentTarget);
+// })
+
+// document.querySelector('.nav').addEventListener('click', function(e) {
+//   this.style.backgroundColor = randomColor();
+//   console.log('NAV', e.target,  e.currentTarget);
+// })
+
+//192. Event delegation

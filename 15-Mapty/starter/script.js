853719a24c0ec627dad4d0f78885f5996f84f0f0
Romain QUERE <romain.quere29@gmail.com>
2022-11-14 21:35:47 +0100

    Mapty project :retrieve coordonates and insert openstreet map

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -10,3 +10,28 @@ const inputDistance = document.querySelector('.form__input--distance');
 const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
+
+if(navigator.geolocation) {
+navigator.geolocation.getCurrentPosition(function (position) {
+    console.log(position);
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    console.log(latitude, longitude);
+    console.log(`https://www.google.fr/maps/@${latitude},${longitude}z`);
+
+    var map = L.map('map').setView([latitude, longitude], 13);
+
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+    }).addTo(map);
+
+    L.marker([latitude, longitude]).addTo(map)
+        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+        .openPopup();
+
+}, 
+function () {
+    alert('Could not get your position');
+})
+}
+

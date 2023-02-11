13725531ddcc4efa6e80b17e6877709955e40c4f
Romain QUERE <rquere@tribvn-hc.com>
2023-02-11 10:16:37 +0100

    Refactorize js code in MVC model

---
+++ 18-forkify/starter/src/js/helpers.js
@@ -0,0 +1,23 @@
+import {TIMEOUT_SEC} from "./config";
+
+const timeout = function (s) {
+    return new Promise(function (_, reject) {
+      setTimeout(function () {
+        reject(new Error(`Request took too long! Timeout after ${s} second`));
+      }, s * 1000);
+    });
+  };
+
+export const getJSON = async function(url) {
+    try {
+        console.log(url);
+        const res = await Promise.race([fetch(url),timeout(TIMEOUT_SEC)]);
+        // console.log(res);
+        const data = await res.json();
+        // console.log(data);
+        if (!res.ok) throw new Error(`${data.message} (${res.status})`);
+        return data;
+    } catch (err) {
+        throw err;
+    }
+};

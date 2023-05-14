d6579c7cf5f40f8cb70a16c942df40423127a96a
Romain QUERE <rquere@tribun.health>
2023-05-14 11:51:06 +0200

    substitute Franction module by Fracty

--- 18-forkify/starter/src/js/views/recipeView.js
+++ 18-forkify/starter/src/js/views/recipeView.js
@@ -1,7 +1,8 @@
 import View from './View';
 
 import icons from 'url:../../img/icons.svg';
-import {Fraction} from 'fractional';
+// import {Fraction} from 'fractional';
+import fracty from 'fracty';
 
 class RecipeView extends View {
     _parentElement = document.querySelector('.recipe');
@@ -126,7 +127,7 @@ class RecipeView extends View {
             <svg class="recipe__icon">
               <use href="${icons}#icon-check"></use>
             </svg>
-            <div class="recipe__quantity">${ing.quantity ? new Fraction(ing.quantity).toString() : ''}</div>
+            <div class="recipe__quantity">${ing.quantity ? fracty(ing.quantity).toString() : ''}</div>
             <div class="recipe__description">
               <span class="recipe__unit">${ing.unit}</span>
               ${ing.description}
